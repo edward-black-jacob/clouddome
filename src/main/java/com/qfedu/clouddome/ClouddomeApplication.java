151ae35d35e8dfe12c25022cda953d64cc606b8a
hello <278475128@qq.com>
2020-11-03 21:00:58 +0800

    模块初始化0.1

---
+++ src/main/java/com/qfedu/clouddome/ClouddomeApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.clouddome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouddomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qfedu/clouddome/ClouddomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qfedu.clouddome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouddomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
